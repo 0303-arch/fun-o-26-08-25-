16484531de444f158d1aa850ae03340a440c3067
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-08-26 22:19:38 -0300

    Add files via upload

---
+++ funçao.php
@@ -0,0 +1,33 @@
+<?php
+	
+//$num1=30; 
+//$num2=50;
+
+
+
+	function somaValor($num1, $num2){
+		$varSoma= $num1 + $num2;
+		$varSubtracao= $num1 - $num2;
+		$varDivisao= $num1 / $num2;
+		$varMultiplica= $num1 * $num2;
+
+	  if (somaValor == $varSoma);
+	  	if (somaValor == $varSubtracao); {
+	  		
+	  	}
+
+
+	else;
+
+}
+
+
+
+
+
+
+
+
+	
+
+?>
