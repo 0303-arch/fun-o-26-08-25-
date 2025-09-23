3c515ade2f0b3267b9f699162d473110db55a433
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-09-23 20:22:35 -0300

    Add files via upload

---
+++ array.php
@@ -0,0 +1,30 @@
+<?php
+
+	/*$alunos=array();
+
+	$alunos[0]="Maria";
+	$alunos[1]="José";
+
+	echo($alunos[0].$alunos[1]);
+
+	$nomes = array("Maria"," José"," Pedro");
+
+	echo($nomes[0] . $nomes[1] . $nomes[2]);
+
+	$email[] = "mariahotmail@hotmail.com";
+	$email[] = "josehotmail@hotmail.com";
+	$email[] = "joaohotmail@hotmail.com";
+
+	print_r($email);
+	var_dump($email);
+*/
+	for ($i=0 ; $i < 7 ; $i++){
+		$vetor[$i] = 12;
+	}
+
+	for ($i=0 ; $i < 7 ; $i++){
+		echo "O valor do vetor: ". $vetor[$i] . "<br/>";	
+
+	}
+
+?>
