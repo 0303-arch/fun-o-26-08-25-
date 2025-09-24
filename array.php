9ed1f4bc1e0e80f49727e152640c46748fd8f4c7
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-09-23 22:11:48 -0300

    Update array.php
    Atualização de exemplos de arrays php

--- array.php
+++ array.php
@@ -17,7 +17,7 @@
 
 	print_r($email);
 	var_dump($email);
-*/
+
 	for ($i=0 ; $i < 7 ; $i++){
 		$vetor[$i] = 12;
 	}
@@ -26,5 +26,68 @@
 		echo "O valor do vetor: ". $vetor[$i] . "<br/>";	
 
 	}
+
+	/*	
+	$nome1 = "Maria";
+	$nome2 = "João";
+	$nome3 = "Pedro";
+	$nome4 = "Paulo";
+	$nome5 = "Lucas";
+
+	echo("$nome1<br/>$nome2<br/>$nome3<br/>$nome4<br/>$nome5<br/>");
+
+
+	$nome = array("Maria","João","Pedro","Paulo","Lucas");
+
+	echo "Os nomes no array:</br>";
+
+	foreach ($nome as $nomes) {
+		echo "</br>".$nomes;
+	}
+
+	$num = array(1,2,3,4,5);
+
+	foreach ($num as $valor) {
+		echo "Valor é $valor <br/>";
+	}
+
+	$num["U"] = "Um";
+	$num["DO"] = "Dois";
+	$num["TR"] = "Três";
+	$num["QU"] = "Quatro";
+	$num["CI"] = "Cinco";
+
+	foreach($num as $valor){
+		echo "Valor é $valor <br/>";
+
+	}
+
+
+	echo "<br/>";
+
+	$nome["MA"] = "Maria";
+	$nome["ZE"] = "José";
+	$nome["PE"] = "Pedro";
+
+
+	echo("<pre>");
+	print_r($nome);
+	echo("<pre>");
+
+
+	$salario = $array = array("João" => 2000, "Pedro" => 1000, "Maria" => 500);
+
+	echo "Salário de João é: " . $salario['João'] . "<br/>";
+    echo "Salário de Pedro é: " . $salario['Pedro'] . "<br/>";
+    echo "Salário de Maria é: " . $salario['Maria'] . "<br/>" ."<br/>";
+
+    $salario['João'] = "Alto";
+    $salario['Pedro'] = "Médio";
+    $salario['Maria'] = "Baixo";
+
+    echo "Salário de João é: " . $salario['João'] . "<br/>";
+    echo "Salário de Pedro é: " . $salario['Pedro'] . "<br/>";
+    echo "Salário de Maria é: " . $salario['Maria'] . "<br/>";*/
+
 
 ?>
