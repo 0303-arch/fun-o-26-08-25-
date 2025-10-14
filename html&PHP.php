d0a29479802b09e4d8e6a66c7fcb617fdbe78365
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-10-14 20:13:04 -0300

    Add files via upload

---
+++ html&PHP.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário</title>
+</head>
+<body>
+	<h1>Formulário de Cadastro</h1>
+	<form action="Cadastrar.php" method="get">
+		<label for="nome">Nome:</label>
+		<input type="text" name="Nome" id="nome">
+		<br>
+		<br>
+		<label for="email">Email:</label>
+		<input type="text" name="Email" id="email">
+		<br>
+		<br>
+		<input type="submit" name="enviar">
+		<button type="submit">Cadastrar</button>
+
+</body>
+</html>
