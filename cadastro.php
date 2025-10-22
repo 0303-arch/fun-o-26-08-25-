12775f843fd299662a35349fad36354a62957093
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-10-21 22:12:15 -0300

    Add files via upload

--- cadastro.php
+++ cadastro.php
@@ -1,14 +1,21 @@
 <?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'post'){
+
 	$primeiroNome = $_POST['$primeiroNome'];
 	$segundoNome = $_POST['$segundoNome'];
 	$escolhasPets = $_POST['$escolhasPets'];
 
 	echo($primeiroNome .
 		$segundoNome .
-		$escolhasPets
+		$escolhasPets);
 
+	echo '<a href="../index.php">Voltar</a>';
 
-);
+	//header("location: ../index.php")
+}else{
+	echo "É get"
+}
 
 
 ?>
