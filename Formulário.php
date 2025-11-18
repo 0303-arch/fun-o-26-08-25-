9361e204fca285321ae60a502c45b9dddf6b70a5
0303-arch <kanbarajoaolopes2000@gmail.com>
2025-11-18 20:20:00 -0300

    Add files via upload

--- Formulário.php
+++ Formulário.php
@@ -3,9 +3,76 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Qual é o melhor time?</title>
+	<title>Qual é a melhor Escuderia da Fórmula 1?</title>
 </head>
 <body>
+	<div class="container">
+		
+		<form action="#" method="get">
+
+			<h1>Qual é a melhor Escuderia da Fórmula 1?</h1>
+			
+			<input type="radio" name="Escuderia" value="Ferrari">
+			<label>Ferrari</label>
+			<br>
+			<input type="radio" name="Escuderia" value="RedBull">
+			<label>RedBull</label>
+			<br>
+			<input type="radio" name="Escuderia" value="Mercedes">
+			<label>Mercedes</label>
+			<br>
+			<input type="radio" name="Escuderia" value="McLaren">
+			<label>McLaren</label>
+			<br>
+			<input type="radio" name="Escuderia" value="AstonMartin">
+			<label>Aston Martin</label>
+			<br>
+			<input type="radio" name="Escuderia" value="Alpine">
+			<label>Alpine</label>
+			<br>
+			<input type="radio" name="Escuderia" value="Willians">
+			<label>Willians</label>
+			<br>
+			<input type="radio" name="Escuderia" value="RacingBulls">
+			<label>RacingBulls</label>
+			<br>
+			<input type="radio" name="Escuderia" value="Haas">
+			<label>Haas</label>
+			<br>
+			<input type="radio" name="Escuderia" value="KickSauber">
+			<label>Kick Sauber</label>
+
+			<input type="submit" name="Enviar">
+
+
+
+
+
+		</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
 
 </body>
 </html>
